aaf352cf717933ae77ca9d1d8c674830107c6d4d
mashadab@mit.edu <afzalshadab0@yahoo.com>
2021-02-14 21:11:05 -0600

    GUI tutorial: running all the required apps at once

---
+++ GUI_tutorials/Run important programs/run_important_programs.py
@@ -0,0 +1,50 @@
+# Creating a GUI from Python program
+# Run important programs
+# author: Mohammad Afzal Shadab
+# email: mashadab@utexas.edu
+# date: 14 February 2021
+# $ pip install tk
+# $ pip install TCL
+
+import tkinter as tk  #for creating the GUI
+from tkinter import filedialog, Text 
+import os             #allows to run the apps
+
+# Initializing a root
+root = tk.Tk()  #holds the app like a body like HTML
+apps = []       #placeholder for all files that we are adding
+
+# Defining a function to add the apps to a list
+def addApp():
+    #widget which attaches to everything in the frame
+    for widget in frame.winfo_children(): #to delete information in the frame everytime
+        widget.destroy()
+    
+    filename = filedialog.askopenfilename(initialdir="/",title="Select File",
+    filetypes=(("executables","*.exe"), ("all files", "."))) #allow us to initiliaze app attributes
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+# Defining a function to run the apps in the list
+
+# Adding a canvas
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+# Adding a frame    
+frame = tk.Frame(root, bg ="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)   
+
+#Adding buttons
+openFile = tk.Button(root, text="Open File", padx=10, 
+                     pady=5, fg="white", bg="#263D42", command=addApp)  
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, 
+                     pady=5, fg="white", bg="#263D42")  
+runApps.pack()
+
+root.mainloop()
